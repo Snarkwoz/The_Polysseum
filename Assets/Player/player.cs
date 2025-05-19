92069d420186aad5e7fa7d9e12625b8466512191
Snarkwoz <140124176+Snarkwoz@users.noreply.github.com>
2025-05-19 10:39:15 +1000

    Added functions for moving up, dow, left and right

--- Assets/Player/player.cs
+++ Assets/Player/player.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     public Rigidbody2D body;
+    public int speed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,6 +14,26 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            body.linearVelocity = Vector2.up * speed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A) == true)
+        {
+            body.linearVelocity = Vector2.left * speed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S) == true)
+        {
+            body.linearVelocity = Vector2.down * speed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) == true)
+        {
+            body.linearVelocity = Vector2.right * speed;
+        }
         
+
     }
 }
