194d96ef42ee9a215aa78d8a8e7dfa44a1588847
Snarkwoz <140124176+Snarkwoz@users.noreply.github.com>
2025-05-23 12:38:35 +1000

    Finished game ove screen, added function for death but have no way to trigger it, adjusted positioning and scale of floor and walls

--- Assets/Scripts/LogicScript.cs
+++ Assets/Scripts/LogicScript.cs
@@ -3,8 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
+    public GameObject gameoverscreen;
+
     public void restartgame()
-    { 
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
+    public void gameover()
+    { 
+        gameoverscreen.SetActive(true); 
+    }
+
 }
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -4,6 +4,8 @@ public class player : MonoBehaviour
 {
     public Rigidbody2D body;
     public int speed;
+    public LogicScript logic;
+    public bool alive = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,26 +16,31 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) == true && alive == true)
         {
             body.linearVelocity = Vector2.up * speed;
         }
 
-        if (Input.GetKeyDown(KeyCode.A) == true)
+        if (Input.GetKeyDown(KeyCode.A) == true && alive == true)
         {
             body.linearVelocity = Vector2.left * speed;
         }
 
-        if (Input.GetKeyDown(KeyCode.S) == true)
+        if (Input.GetKeyDown(KeyCode.S) == true && alive == true)
         {
             body.linearVelocity = Vector2.down * speed;
         }
 
-        if (Input.GetKeyDown(KeyCode.D) == true)
+        if (Input.GetKeyDown(KeyCode.D) == true && alive == true)
         {
             body.linearVelocity = Vector2.right * speed;
         }
         
+    }
 
+    private void death(Collision2D collision)
+    {
+        logic.gameover();
+        alive = false;
     }
 }
