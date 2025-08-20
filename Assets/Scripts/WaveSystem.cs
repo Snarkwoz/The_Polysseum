9e9b1b37c41992417905979eeafaa0d005a66f8e
Snarkwoz <140124176+Snarkwoz@users.noreply.github.com>
2025-08-20 10:36:46 +1000

    Some progress
    Changed the position of and added some death planes and the floor. Started work on the wave management system.

---
+++ Assets/Scripts/WaveSystem.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class WaveSystem
+{
+    public GameObject box_enemy;
+    // public GameObject another_enemy_maybe;
+    public int wavecounter = 1;
+
+    void Update()
+    {
+        // generate_wave()
+        
+        // spawn in enemies
+        // if more than x enemies, stop spawning
+
+        // if list is empty and all enemies defeated, start again
+    }
+
+    private void generate_wave()
+    {
+        if (wavecounter == 1)
+        {
+            
+            //wave.Add(stuff);
+        }
+    }
+    
+}
